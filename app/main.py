d19c89971ce22eee572f83c9b07d5c077950b118
Tomasz Szatałowicz <szatalowicztomek@gmail.com>
2025-10-18 11:31:14 +0200

    quest

--- app/main.py
+++ app/main.py
@@ -1,6 +1,16 @@
-from typing import Callable
+from typing import Callable, Any
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    results = {}
+
+    def wrapper(*args: Any, **kwargs: Any) -> Any:
+        key = (args, tuple(sorted(kwargs.items())))
+        if key not in results:
+            print("Calculating new result")
+            results[key] = func(*args, **kwargs)
+        else:
+            print("Getting from cache")
+        return results[key]
+
+    return wrapper
