5e97b7589671075cb90d6ed8ba2b5b6f43c70c21
Tomasz Szatałowicz <szatalowicztomek@gmail.com>
2025-10-21 09:45:55 +0200

    Task corrected. Added functools.wraps and improved code structure.

--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
 from typing import Callable, Any
-
+from functools import wraps
 
 def cache(func: Callable) -> Callable:
     results = {}
-
+    @wraps(func)
     def wrapper(*args: Any, **kwargs: Any) -> Any:
         key = (args, tuple(sorted(kwargs.items())))
         if key not in results:
